6e9790a9b32d4ace0c7becc50538da3953ee94fc
Camila <100419816@alumnos.uc3m.es>
2020-03-05 12:27:40 +0100

    Some fixes on the plotly and selection

--- Take-Away/app.R
+++ Take-Away/app.R
@@ -8,9 +8,8 @@ library(dplyr)
 library(jsonlite)
 library(shinyjs)
 
-#library(RCurl)
-#getURL
-#503072804
+#############################################################################
+#Get data from the web
 
 url<- "https://pokemon-go1.p.rapidapi.com/pokemon_stats.json"
 key = "11a0831306msh85289b71ee29d28p1b0d2ajsnd56ec6de06bd"
@@ -23,6 +22,9 @@ content(pokemon)
 
 details<- httr::content(pokemon,as='parsed')
 details
+
+##########################################################################
+#cleaning, manipulation and analysis of the retrieved data
 
 attack <- numeric(length(details))
 defense<- numeric(length(details))
@@ -45,19 +47,22 @@ data<- distinct(data)
 
 data_name<- levels(data$name)
 
+##########################################################################3
+
+
 headerRow <- div(id="header", useShinyjs(),
                  selectInput("selpok", 
                              label="Select the Pokemon", 
                              multiple = TRUE,
                              choices=data_name))
-
 plotlyPanel <- tabPanel("Plotly",
                         plotly::plotlyOutput("plotlyData")
 )
 
 
-ui <- navbarPage("Pokemon App",
-    plotlyPanel, 
+ui <- navbarPage(
+    titlePanel=("Pokemon App"),
+    plotlyPanel,
     id = "navBar",
     header=headerRow
 )
@@ -65,20 +70,25 @@ ui <- navbarPage("Pokemon App",
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-observe(if(input$navBar=="Map") {
-    cat(file=stderr(), input$navBar, "\n")
+observe({if(input$navBar=="Map") {
     shinyjs::hide("header")
 } else {
-    cat(file=stderr(), input$navBar, "\n")
     shinyjs::show("header")
+}
 })
 
+data_filtered <- reactive({
+    req(input$selpok)
+    req(input$stamina)
+    data %>% filter(name %in% input$selpok, 
+                         stamina == input$stamina)
+})   
 
 output$plotlyData <- plotly::renderPlotly({
-ggplot(aes(x=name, y=stamina, fill=name)) +
+ggplot(data_filtered(), aes(x=name, y=stamina, fill=name)) +
         geom_bar(stat="identity", position=position_dodge())
 })
-}
+} 
 
 # Run the application 
 shinyApp(ui = ui, server = server)

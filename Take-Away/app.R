1b7b39f90162d401d425c4fe0bf613c375ff3fe5
Camila <100419816@alumnos.uc3m.es>
2020-03-09 10:23:38 +0100

    other changes

--- Take-Away/app.R
+++ Take-Away/app.R
@@ -10,41 +10,42 @@ library(shinyjs)
 #############################################################################
 #Get data from the web
 
-url<- "https://pokemon-go1.p.rapidapi.com/pokemon_stats.json"
-key = "11a0831306msh85289b71ee29d28p1b0d2ajsnd56ec6de06bd"
+#url<- "https://pokemon-go1.p.rapidapi.com/pokemon_stats.json"
+#key = "11a0831306msh85289b71ee29d28p1b0d2ajsnd56ec6de06bd"
 
-pokemon = GET(url, config=add_headers("x-rapidapi-host"= "pokemon-go1.p.rapidapi.com",'x-rapidapi-key' = key))
+#pokemon = GET(url, config=add_headers("x-rapidapi-host"= "pokemon-go1.p.rapidapi.com",'x-rapidapi-key' = key))
 #load("pokemon_API.RData")
-content(pokemon)
+#content(pokemon)
 
 
-details<- httr::content(pokemon,as='parsed')
-details
+#details<- httr::content(pokemon,as='parsed')
+#details
 
 ##########################################################################
 #cleaning, manipulation and analysis of the retrieved data
 
-attack <- numeric(length(details))
-defense<- numeric(length(details))
-stamina <- numeric(length(details))
-formas <- numeric(length(details))
-id <- numeric(length(details))
-name <- numeric(length(details))
+# attack <- numeric(length(details))
+# defense<- numeric(length(details))
+# stamina <- numeric(length(details))
+# formas <- numeric(length(details))
+# id <- numeric(length(details))
+# name <- numeric(length(details))
+# 
+# for (i in 1:length(details)) {
+#     attack[i] <- as.numeric(details[[i]]$base_attack) 
+#     defense[i] <- as.numeric(details[[i]]$base_defense)
+#     stamina[i] <- as.numeric(details[[i]]$base_stamina)
+#     formas<- details[[i]]$form
+#     id[i] <- as.numeric(details[[i]]$pokemon_id)
+#     name[i] <- details[[i]]$pokemon_name
+# }
+# 
+# data<- as.data.frame(cbind(id, name, formas, stamina, defense, attack))
+# data<- distinct(data)
 
-for (i in 1:length(details)) {
-    attack[i] <- as.numeric(details[[i]]$base_attack) 
-    defense[i] <- as.numeric(details[[i]]$base_defense)
-    stamina[i] <- as.numeric(details[[i]]$base_stamina)
-    formas<- details[[i]]$form
-    id[i] <- as.numeric(details[[i]]$pokemon_id)
-    name[i] <- details[[i]]$pokemon_name
-}
-
-data<- as.data.frame(cbind(id, name, formas, stamina, defense, attack))
-data<- distinct(data)
-
+load("data.RData")
 data_name<- sort(data$name)
-#load("data.RData")
+
 
 ##########################################################################3
 
@@ -57,7 +58,7 @@ headerRow <- div(id="header", useShinyjs(),
                  downloadButton("report", "Generate report")
 )
 
-plotlyPanel <- tabPanel("Plotly",
+plotlyPanel <- tabPanel("Plotly stamina",
                         plotly::plotlyOutput("plotlyData")
 )
 
@@ -72,42 +73,43 @@ ui <- navbarPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-observe({if(input$navBar=="Map") {
-    shinyjs::hide("header")
-} else {
-    shinyjs::show("header")
-}
-})
+    observe({if(input$navBar=="Map") {
+        shinyjs::hide("header")
+    } else {
+        shinyjs::show("header")
+    }
+    })
 
-data_filtered <- reactive({
-    req(input$selpok)
-    req(input$stamina)
-    data %>% filter(nam %in% input$selpok, 
-                         stamina %in% input$stamina)
-})   
-
-output$plotlyData <- plotly::renderPlotly({
-ggplot(data_filtered(), aes(x=nam, y=stamina, fill=nam)) +
-        geom_bar(stat="identity", position=position_dodge())
-})
-output$report <- downloadHandler(
-# For PDF output, change this to "report.pdf"
-filename = "report.pdf",
-content = function(file) {
-    tempReport <- file.path(tempdir(), "report.Rmd")
-    file.copy("report.Rmd", tempReport, overwrite = TRUE)
+    data_filtered <- reactive({
+        req(input$selpok)
+        req(input$stamina)
+        data %>% filter(name %in% input$selpok,
+                             stamina == input$stamina)
+    })
+    # 
     
-    # Set up parameters to pass to Rmd document
-    params <- list(
-        selPok = isolate(input$selpok),
-        stamina = isolate(input$stamina)
-    )
-
-    rmarkdown::render(tempReport, output_file = file,
-                      params = params,
-                      envir = new.env(parent = globalenv())
-    )
-})
+    output$plotlyData <- plotly::renderPlotly({
+    ggplot(data_filtered, aes(x=name, y=stamina, fill=name)) +
+            geom_bar(stat="identity", position=position_dodge())
+    })
+    output$report <- downloadHandler(
+    # For PDF output, change this to "report.pdf"
+    filename = "takeaway.pdf",
+    content = function(file) {
+        tempReport <- file.path(tempdir(), "takeaway.Rmd")
+        file.copy("takeaway.Rmd", tempReport, overwrite = TRUE)
+        
+        # Set up parameters to pass to Rmd document
+        params <- list(
+            selPok = isolate(input$selpok),
+            stamina = isolate(input$stamina)
+        )
+    
+        rmarkdown::render(tempReport, output_file = file,
+                          params = params,
+                          envir = new.env(parent = globalenv())
+        )
+    })
 
 } 
 
